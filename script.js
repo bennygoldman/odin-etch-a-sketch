7f571c699ab9497b3ddb9b90f81d16dec6342b5c
bennygoldman <goldmanb@gmail.com>
2023-06-12 14:58:38 -0400

    complete basic functionality of custom grid prompt
    everything works to the letter of the assignment. need to come back and style + do extra credit.

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 const container = document.querySelector('.container');
 const resetButton = document.querySelector('.reset');
-let gridSize = 256;
+resetButton.addEventListener('click', resetGrid);
 
-for (let i = 0; i < gridSize; i++) {
-    const squareDiv = document.createElement('div');
-    squareDiv.classList.add('square');
-    squareDiv.addEventListener('mouseenter', (e) => {
-        e.target.style.backgroundColor = "black";
-    })
-    container.appendChild(squareDiv);
-}
 
 function resetGrid() {
     let choice = prompt('pick a number 1-100');
@@ -17,17 +9,31 @@ function resetGrid() {
     if (newGrid > 100 || newGrid < 1) {
         return resetGrid();
     } else {
-        // run clearGrid()
-        gridSize = newGrid;
-        //run buildGrid()
+        clearGrid();
+        initalizeGrid(newGrid);
     }
 
 }
 
 function clearGrid() {
-
+    let pixels = container.querySelectorAll('.pixel');
+    pixels.forEach(pixel => pixel.remove())
 }
 
-function buildGrid() {
+function initalizeGrid(number) {
+    let gridArea = (number * number);
+    for (let i = 0; i < gridArea; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixel.style.width = `${640 / number}px`;
+        pixel.style.height = `${640 / number}px`;
+        container.appendChild(pixel);
+    }
 
+    let pixels = container.querySelectorAll('.pixel');
+    pixels.forEach(pixel => pixel.addEventListener('mouseenter', (e) => {
+        e.target.style.backgroundColor = "black";
+    }))
 }
+
+initalizeGrid(16);
